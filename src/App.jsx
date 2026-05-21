ec475a6b3612dcbaf5ad2bc7db785c8dea2389e5
gurmehakk20 <125135241+gurmehakk20@users.noreply.github.com>
2026-05-22 00:05:19 +0530

    Remove Clients component from App
    Removed Clients component from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import About from './sections/About.jsx';
 import Footer from './sections/Footer.jsx';
 import Navbar from './sections/Navbar.jsx';
 import Contact from './sections/Contact.jsx';
-import Clients from './sections/Clients.jsx';
 import Projects from './sections/Projects.jsx';
 import WorkExperience from './sections/Experience.jsx';
 import Resume from './sections/Resume.jsx'; 
@@ -16,7 +15,6 @@ const App = () => {
       <About />
       <Projects />
       <Resume />
-      <Clients />
       <WorkExperience />
       <Contact />
       <Footer />
